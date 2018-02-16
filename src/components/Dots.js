7ff87d226f0b4f380de9932b7d4b138a5d110cd9
Corey <lewisc503@gmail.com>
2018-02-15 22:10:21 -0800

    init commit

---
+++ src/components/Dots.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { imageList } from "./imageList";
+
+const Dots = props => {
+  return (
+    <div className="dots">
+      {imageList.map((val, idx) => {
+        return idx === props.data ? (
+          <div
+            key={idx}
+            style={{
+              width: "10px",
+              height: "10px",
+              margin: "0 5px 0 5px",
+              borderRadius: "100%",
+              display: "inline-block",
+              backgroundColor: "red",
+              boxShadow: "0 0 5px 2px rgba(255,0,0, 0.6)"
+            }}
+          />
+        ) : (
+          <div
+            key={idx}
+            style={{
+              width: "10px",
+              height: "10px",
+              margin: "0 5px 0 5px",
+              borderRadius: "100%",
+              display: "inline-block",
+              backgroundColor: "white"
+            }}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default Dots;
---
+++ src/components/imageList.js
@@ -0,0 +1,8 @@
+export const imageList = [
+  "https://i.ytimg.com/vi/XM23h-eJ08A/maxresdefault.jpg",
+  "https://s3.amazonaws.com/sidearm.sites/georgiadogs.com/images/2017/10/8/DSC_3477.JPG",
+  "https://cdn-s3.si.com/s3fs-public/styles/marquee_large_2x/public/2017/06/08/rose-lavelle-uswnt-sweden.jpg?itok=Za3fO2bL",
+  "https://excelsior.com/victorville/wp-content/uploads/2017/02/img_2986.jpg",
+  "https://udemy-images.udemy.com/course/750x422/691786_7977.jpg",
+  "https://www.islands.com/sites/islands.com/files/styles/1000_1x_/public/images/2016/02/tunnels_beach_kauai.jpg?itok=b-lnK-wq"
+];
